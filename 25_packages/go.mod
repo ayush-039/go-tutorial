65b3fdc1a1e4489feab2207a9039c0bddcff36c7
ayush-039 <ayushabarot3902@gmail.com>
2025-02-28 17:09:28 +0530

    add the packages

---
+++ 25_packages/go.mod
@@ -0,0 +1,3 @@
+module github.com/ayush-039/go-tutorial
+
+go 1.22.2
--- 25_packages/main.go
+++ 25_packages/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/ayush-039/go-tutorial/auth"
+	"github.com/ayush-039/go-tutorial/user"
+)
+
+func main() {
+	auth.LoginWithCredientials("ayush Barot", "ayush1234")
+	session := auth.GetSession()
+	fmt.Println("session:", session)
+
+	user := user.User{
+		Email: "ayushabarot3902@gmail.com",
+		Name:  "Ayush Barot",
+	}
+
+	fmt.Println(user)
 }
